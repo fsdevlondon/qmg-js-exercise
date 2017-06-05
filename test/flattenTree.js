35262fdc1953f8dbbdcfc7c593234dc58eeb2055
Marc Rivelles <rivelles.marc@gmail.com>
2017-06-05 11:55:46 +0100

    Added first essential test

--- test/flattenTree.js
+++ test/flattenTree.js
@@ -1,3 +1,11 @@
 const test = require('ava');
+const flattenTree = require('../lib/flattenTree');
+const answers = require('../examples/answers.json')
+const expectedOutput = require('../examples/expectedOutput.json')
+const questionSet = require('../examples/questionset.json')
 
-test.todo('flattens correctly the provided examples');
+
+test('flattens correctly the provided examples', t => {
+  const result = flattenTree(questionSet, answers)
+  t.deepEqual(result, expectedOutput)
+});
